be875f1db4aa1c344e5f57ba8dc95ed5d16a3705
Daria_Yakymova <yakymovadaria@gmail.com>
2023-11-21 17:19:26 +0200

    Added files for selenium grid

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 import os
 
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
-# HUB_URL = "http://localhost:4444/wd/hub"
-HUB_URL = "http://test:123@localhost:4444/wd/hub"
+HUB_URL = "http://localhost:4444/wd/hub"
+# HUB_URL = "http://test:123@localhost:4444/wd/hub"
 
