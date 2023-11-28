f6fcb46131fcc2f597112db4d89fd2d3f274bdb7
Daria_Yakymova <yakymovadaria@gmail.com>
2023-11-28 17:51:24 +0200

    change hub for connection between ggr and jenkins

--- constants.py
+++ constants.py
@@ -1,6 +1,7 @@
 import os
 
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
-HUB_URL = "http://localhost:4444/wd/hub"
+# HUB_URL = "http://localhost:4444/wd/hub"
+HUB_URL = "http://192.168.0.104:4444/wd/hub"
 # HUB_URL = "http://test:123@localhost:4444/wd/hub"
 
